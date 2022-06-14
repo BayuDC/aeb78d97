90e90167b2ff5ade95d806518fec90909612331c
BayuDC <dev.bayudc@gmail.com>
2022-06-14 13:29:33 +0700

    chore: basic setup

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script>
+export default { name: 'App' };
+</script>
+
+<template>
+    <router-view />
+</template>
+
+<style lang="scss">
+* {
+    margin: 0;
+    padding: 0;
+}
+</style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,9 @@
+<script>
+export default {
+    name: 'HomePage',
+};
+</script>
+
+<template>
+    <h1>Home</h1>
+</template>
