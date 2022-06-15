1d2970965d6a1f996bff3f0f99c9176b4d48cb3e
BayuDC <dev.bayudc@gmail.com>
2022-06-15 15:16:51 +0700

    chore: add container

--- src/pages/Template.vue
+++ src/pages/Template.vue
@@ -11,5 +11,32 @@ export default {
 
 <template>
     <Header />
-    <router-view />
+    <main>
+        <div class="container">
+            <router-view />
+        </div>
+    </main>
 </template>
+
+<style lang="scss">
+@import '../styles/variable';
+@import '../styles/mixin';
+main {
+    .container {
+        margin: 30px auto;
+        max-width: 1024px;
+        padding: 0 20px;
+
+        @include on-tablet {
+            padding: 0 30px;
+        }
+        @include on-desktop {
+            padding: 0 40px;
+        }
+    }
+    h1 {
+        font-family: $poppins;
+        font-size: 24px;
+    }
+}
+</style>
