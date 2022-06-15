c5a772e832317fe4bb2b5dde483cf2baad675a95
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2022-06-15 15:55:15 +0700

    initial album page set up

---
+++ src/components/AlbumsItem.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { defineComponent } from 'vue';
+</script>
+
+<template>
+    <!--  -->
+</template>
+
+<script>
+export default defineComponent({
+    name: 'AlbumsItem',
+});
+</script>
+
+<style lang='scss'>
+@import '../styles/mixin';
+@import '../styles/variable';
+</style>
---
+++ src/pages/Album.vue
@@ -0,0 +1,20 @@
+<script setup>
+import AlbumsItem from '../components/AlbumsItem';
+import { defineComponent } from 'vue';
+</script>
+
+<template>
+    <!--  -->
+</template>
+
+<script>
+export default defineComponent({
+    name: 'Albums',
+    components: { AlbumsItem },
+});
+</script>
+
+<style lang='scss'>
+@import '../styles/mixin';
+@import '../styles/variable';
+</style>
