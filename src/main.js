7e47f5570b01bf0058a3525055b98dc85ebb0c7e
BayuDC <dev.bayudc@gmail.com>
2022-06-18 09:08:04 +0700

    chore: remove vue-axios plugin

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
-import VueAxios from 'vue-axios';
 import router from './router';
 import axios from './axios';
 import App from './App.vue';
 
 const app = createApp(App);
 
+app.config.globalProperties.$http = axios;
+
 app.use(router);
-app.use(VueAxios, axios);
 app.use(createPinia());
 
 app.mount(document.getElementById('app'));
