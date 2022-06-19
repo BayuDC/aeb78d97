c6af72732c9c89c5ce0df23ef66fb511c10d0ff0
BayuDC <dev.bayudc@gmail.com>
2022-06-19 09:54:18 +0700

    feat: coming soon component

---
+++ src/components/ComingSoon.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+    name: 'ComingSoon',
+};
+</script>
+<template>
+    <div class="coming-soon">
+        <h1>Coming Soon</h1>
+    </div>
+</template>
+
+<style lang="scss">
+@import '../styles/variable';
+
+.coming-soon {
+    background: $yellow;
+    padding: 60px;
+    font-family: $poppins;
+    text-align: center;
+}
+</style>
