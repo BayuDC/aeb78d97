b8e166bd619461e05a49902b7e25418bd467efab
BayuDC <dev.bayudc@gmail.com>
2022-06-25 10:17:40 +0700

    feat: load single album data from api

--- src/pages/AlbumDetail.vue
+++ src/pages/AlbumDetail.vue
@@ -1,14 +1,40 @@
 <script>
+import Loading from '../components/Loading.vue';
 import ContentHead from '../components/ContentHead.vue';
 
 export default {
     name: 'AlbumDetail',
     components: {
+        Loading,
         ContentHead,
+    },
+    data() {
+        return {
+            loading: true,
+            album: {},
+        };
+    },
+    created() {
+        const id = this.$route.params.id;
+
+        this.$http
+            .get('/albums/' + id)
+            .then(res => {
+                this.album = res.data.album;
+            })
+            .catch(err => {
+                console.log(err);
+            })
+            .finally(() => {
+                this.loading = false;
+            });
     },
 };
 </script>
 
 <template>
-    <ContentHead title="Album Detail" />
+    <Loading v-if="loading" />
+    <template v-else>
+        <ContentHead :title="album.name" />
+    </template>
 </template>
